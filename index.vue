68ccc10ed38d76d31cb5b6c98fb7e96fece10c5b
Rokiya Ripa <rokaiyaripa4029@gmail.com>
2025-01-07 14:26:51 +0600

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,237 @@
+<template>
+
+    <div class="font-['Hind_Siliguri']">
+        <nav class="flex justify-center items-center my-10">
+            <img src="/img/logo.png" alt="logo">
+        </nav>
+
+        <!-- Header Section -->
+
+        <header class="w-[60%] mx-auto">
+
+            <div class="max-w-5xl mx-auto">
+
+                <div class="sm:text-xl md:text-3xl">
+                    <p class="text-center text-[60px] leading-[100px] font-['Hind_Siliguri'] text-[#1B4B36]
+                font-bold">
+                        স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!
+                        "আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</p>
+                </div>
+                <div class="flex justify-center items-center my-[50px]">
+                    <span
+                        class=" bg-[#EBFF3D] py-[10px] px-[20px] text-[36px] text-[#202020] font-[600] leading-[52px] rounded-[5px]">আপনার
+                        জীবনকে আরও স্মার্ট করুন।</span>
+                </div>
+
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/eYnaXsjVBk8"
+                    title="Amazing Drawing Tool Project for Beginners | HTML, CSS, JavaScript #html #css #coding #frontend"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+                    class="mx-auto rounded-[20px] w-full aspect-video">
+                </iframe>
+
+                <div class="flex justify-center flex-wrap items-center my-[50px] text-[#EBFF3D] 
+                text-[48px] font-[700] leading-[65px] cursor-pointer">
+                    <button class="py-[16px] px-[30px] bg-[#1B4B36]
+                rounded-full hover:bg-[#1d573e]">অর্ডার করতে ক্লিক করুন
+                        <Icon name="tabler:hand-finger-right" />
+                    </button>
+                </div>
+
+
+            </div>
+        </header>
+
+        <main>
+            <section class="w-full bg-[#FAFFCE] min-h-[1531px] mx-auto">
+                <div class="w-[75%] mx-auto py-[50px] ">
+                    <h1 class="text-[68.67px] font-[700px] text-[#1B4B36] w-[55%] py-[40px] mb-8">স্মার্টওয়াচের একাধিক
+                        ফিচার
+                        এক নজরে</h1>
+
+                    <div class="grid md:grid-cols-2 sm:grid-cols-1  grid-cols-1  lg:grid-cols-3 gap-8
+                        justify-center items-center cursor-pointer p-6 ">
+
+<!-- Card 1 -->
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] p-15 group sm:hover:bg-[#1B4B36] md:hover:bg-[#1B4B36]
+                        xl:hover:bg-[#1B4B36]  
+                        hover:bg-[#1B4B36] border-[#1B4B36] py-[10px] px-[10px] text-center 
+                        rounded-tl-[35px] rounded-br-[35px] cursor-pointer justify-center">
+                            <h3
+                                class="card__heading  text-[#1B4B36] group-hover:text-[#EBFF3D] my-[15px] text-[20px] font-extrabold">
+                                স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
+                            <p
+                                class="card__contents group-hover:text-[#FAFFCE] text-[#202020] cursor-pointer font-light">
+                                হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+                        </div>
+
+<!-- Card 2 -->
+
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] p-15 group hover:bg-[#1B4B36] border-[#1B4B36] py-[10px] px-[10px] text-center 
+                        rounded-tl-[35px] rounded-br-[35px] cursor-pointer justify-center">
+                            <h3
+                                class="card__heading  text-[#1B4B36] group-hover:text-[#EBFF3D] my-[15px] text-[20px] font-extrabold">
+                                স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
+                            <p
+                                class="card__contents group-hover:text-[#FAFFCE] text-[#202020] cursor-pointer font-light">
+                                হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+                        </div>
+
+<!-- Card 3 -->
+
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] p-15 group sm:hover:bg-[#1B4B36] md:hover:bg-[#1B4B36] 
+                        xl:hover:bg-[#1B4B36] 
+                        hover:bg-[#1B4B36] border-[#1B4B36] py-[10px] px-[10px] text-center 
+                        rounded-tl-[35px] rounded-br-[35px] cursor-pointer justify-center">
+                            <h3
+                                class="card__heading  text-[#1B4B36] group-hover:text-[#EBFF3D] my-[15px] text-[20px] font-extrabold">
+                                ফিটনেস ট্র্যাকিং</h3>
+                            <p
+                                class="card__contents group-hover:text-[#FAFFCE] text-[#202020] cursor-pointer font-light">
+                                স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড।
+                                ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন।
+                                স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+                        </div>
+
+<!-- Card 4 -->
+
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] p-15 group md:hover:bg-[#1B4B36] sm:hover:bg-[#1B4B36]
+                        hover:bg-[#1B4B36] border-[#1B4B36] py-[10px] px-[10px] text-center 
+                        rounded-tl-[35px] rounded-br-[35px] cursor-pointer justify-center">
+                            <h3
+                                class="card__heading  text-[#1B4B36] group-hover:text-[#EBFF3D] my-[15px] text-[20px] font-extrabold">
+                                স্মার্ট নোটিফিকেশন</h3>
+                            <p
+                                class="card__contents group-hover:text-[#FAFFCE] text-[#202020] cursor-pointer font-light">
+                                কল, মেসেজ, এবং অ্যাপ নোটিফিকেশন সরাসরি আপনার হাতে। গুরুত্বপূর্ণ আপডেট কখনোই মিস করবেন
+                                না।</p>
+                        </div>
+
+                        <!-- Card 5 -->
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] p-15 group hover:bg-[#1B4B36] sm:hover:bg-[#1B4B36] md:hover:bg-[#1B4B36] border-[#1B4B36] py-[10px] px-[10px] text-center 
+                        rounded-tl-[35px] rounded-br-[35px] cursor-pointer justify-center">
+                            <h3
+                                class="card__heading  text-[#1B4B36] group-hover:text-[#EBFF3D] my-[15px] text-[20px] font-extrabold">
+                                দীর্ঘস্থায়ী ব্যাটারি লাইফ</h3>
+                            <p
+                                class="card__contents group-hover:text-[#FAFFCE] text-[#202020] cursor-pointer font-light">
+                                একবার চার্জে ৭+ দিন ব্যবহারের সুবিধা। দ্রুত চার্জিং প্রযুক্তি।</p>
+                        </div>
+
+                        <!-- Card 6 -->
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] p-15 group hover:bg-[#1B4B36] md:hover:bg-[#1B4B36] sm:hover:bg-[#1B4B36] border-[#1B4B36] py-[10px] px-[10px] text-center 
+                        rounded-tl-[35px] rounded-br-[35px] cursor-pointer justify-center">
+                            <h3
+                                class="card__heading  text-[#1B4B36] sm:group-hover:text-[#EBFF3D] lg:group-hover:text-[#EBFF3D] 
+                                md:group-hover:text-[#EBFF3D] my-[15px] text-[20px] font-extrabold">
+                                মিউজিক এবং ক্যামেরা নিয়ন্ত্রণ</h3>
+                            <p
+                                class="card__contents group-hover:text-[#FAFFCE] text-[#202020] cursor-pointer font-light">
+                                মিউজিক চালানো বা ক্যামেরা ক্লিক করুন আপনার স্মার্টওয়াচ থেকে।
+                                সহজ এবং সাবলীল ব্যবহারের সুবিধা।</p>
+                        </div>
+
+                    </div>
+                    <!-- Button -->
+
+                    <div class="flex justify-center flex-wrap items-center my-[50px] text-[#EBFF3D] 
+                text-[48px] font-[700] leading-[65px] cursor-pointer">
+                        <button class="py-[16px] px-[30px] bg-[#1B4B36]
+                rounded-full hover:bg-[#1d573e] lg:hover:bg-[#1d573e] sm:hover:bg-[#1d573e]">অর্ডার করতে ক্লিক করুন
+                            <Icon name="tabler:hand-finger-right" />
+                        </button>
+                    </div>
+
+                </div>
+            </section>
+        </main>
+
+        <!-- Section: Customer Experience -->
+        <section class="bg-white text-white py-12">
+            <div class="container mx-auto text-center">
+                <h2 class="text-2xl font-bold mb-8">আমাদের পণ্য নিয়ে গ্রাহকদের অভিজ্ঞতা</h2>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    <!-- Card 1 -->
+                    <div class="py-7 bg-[#1F4E37] rounded-lg gap-2">
+                        <h3 class="text-xl font-bold text-start p-2">সন্তুষ্ট গ্রাহক</h3>
+                        <p class="text-3xl font-bold text-[#EBFF3D] text-end m-2">৩,০০০+</p>
+                        <hr class="w-full">
+                        <p class="text-center text-[12px] p-3">সন্তুষ্ট গ্রাহক আমাদের পণ্যের প্রতি বিশ্বাস রেখেছেন।</p>
+                    </div>
+                    <!-- Card 2 -->
+                    <div class="py-7 bg-[#1F4E37] rounded-lg">
+                        <h3 class="text-xl font-bold text-start p-2">পুনঃক্রয়কারী</h3>
+                        <p class="text-3xl font-bold text-[#EBFF3D] text-end m-2">৪০০+</p>
+                        <hr class="w-full">
+                        <p class="text-center text-[12px] p-3">পুনঃক্রয় করে আমাদের গুণমান নিশ্চিত করেন।</p>
+                    </div>
+                    <!-- Card 3 -->
+                    <div class="py-7 bg-[#1F4E37] rounded-lg">
+                        <h3 class="text-xl font-bold text-start p-2">পণ্য বিক্রি</h3>
+                        <p class="text-3xl font-bold text-[#EBFF3D] text-end m-2">৫০০০+</p>
+                        <hr class="w-full">
+                        <p class="text-center text-[12px] p-3">৫০০০ পণ্যের বিক্রি আমাদের জনপ্রিয়তার প্রমাণ।</p>
+                    </div>
+                    <!-- Card 4 -->
+                    <div class="py-7 bg-[#1F4E37] rounded-lg">
+                        <h3 class="text-xl font-bold text-start p-2">গ্রাহক সন্তুষ্টি</h3>
+                        <p class="text-3xl font-bold text-[#EBFF3D] text-end m-2">৯৫%</p>
+                        <hr class="w-full">
+                        <p class="text-center text-[12px] p-3">৯৫% গ্রাহক আমাদের সেবা ও পণ্যে সন্তুষ্ট।</p>
+                    </div>
+                </div>
+
+                <div class="flex justify-center flex-wrap items-center my-[50px] text-[#1B4B36] 
+                text-[48px] font-[700] leading-[65px] cursor-pointer">
+                    <button class="py-[16px] px-[30px] bg-[#EBFF3D]
+                rounded-full hover:bg-[#d8e941]">অর্ডার করতে ক্লিক করুন
+                        <Icon name="tabler:hand-finger-right" />
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer>
+            <div class="footer-container justify-center flex gap-[50px] items-center p-10 bg-[#0C3200]">
+                <div class="footer-left text-white flex items-center gap-2 w-full">
+                    <Icon name="tabler:phone-call" class="text-[16px]" />
+                    <p class="text-white font-semibold">+880 103 234 350</p>
+                </div>
+
+                <div class="footer-logo items-center w-full">
+                    <img src="/img/foot_logo.png" alt="Footer Logo">
+                </div>
+
+                <div class="footer-right flex justify-end items-center  flex-wrap items-right my-[40px] text-[42px] gap-8 w-full">
+                    <div class="facebook_icon w-14 h-14 bg-white text-center p-1 rounded-full ">
+                        <Icon name="tabler:brand-facebook" />
+                    </div>
+                    <div class="youtybe_icon w-14 h-14 bg-white text-center p-1 rounded-full ">
+                        <Icon name="tabler:brand-youtube-filled" />
+                    </div>
+                    <div class="tictok_icon w-14 h-14 bg-white text-center p-1 rounded-full">
+                        <Icon name="tabler:brand-tiktok" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-end text-center text-[#ebecdd] bg-[#0A2900] h-[60px] p-10">
+                <p>© 2024 All Rights Reserved by <span class="text-[#EBFF3D]">BDFunnelBuilder.</span> Unauthorized use
+                    prohibited.</p>
+            </div>
+        </footer>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
